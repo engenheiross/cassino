fd681f72ce2ccd68079d365f8e4984fe69ec51a2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-17 10:19:46 -0300

    Fazendo a roleta tirar um numero, diminuindo a velocidade gradualmente

--- script.js
+++ script.js
@@ -1,59 +1,86 @@
-var owl = $('.owl-carousel');
+var stoping = false;
+var itemSelected = 0;
 
-let currentItem;
-let rolling = false;
-
-let listItems = 7;
-
-let black_numbers = [2, 4, 6, 8, 10, 11, 13];
-let red_numbers = [1, 3, 5, 7, 9, 12, 14];
-
-owl.owlCarousel({
-    dots: false,
-    loop: true,
-
-    items: listItems,
-    autoplay: false,
+jQuery(function ($) {
+    var $owl = $('.owl-carousel');
     
+    // Inicializando o Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 0,
+        nav: false,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 7
+            }
+        }
+    });
+
+    // Clicar no botão de Rodar
+    $('#rodar').click(function (e) {
+        e.preventDefault();
+        stoping = false;
+        // Numero aleatório entre 0 e 14
+        itemSelected = Math.floor((Math.random() * 14));
+        var $jump = $(this);
+        $jump.html('Rodando ...');
+        $jump.attr('disabled', 'disabled');
+        // Trigger autoplay owl
+        $owl.trigger('play.owl.autoplay', [100]);
+        // Diminuir a velocidade gradualmente
+        setTimeout(slowSpeed, 2000, $owl, 200);
+        setTimeout(slowSpeed, 4000, $owl, 250);
+        setTimeout(slowSpeed, 5000, $owl, 300);
+        setTimeout(stopAutoplay, 6000);
+        return false;
+    });
+
+    // Event changed Owl
+    $owl.on('changed.owl.carousel', function (e) {
+        if (stoping) {
+            var index = e.item.index;
+            var element = $(e.target).find(".owl-stage").find('.number').eq(index);
+            var item = element.html();
+            console.log(element);
+            if (item == itemSelected) {
+                // Se o elemento atual for igual ao numero random, parar a roleta
+                $owl.trigger('stop.owl.autoplay');
+                $('#rodar').html('Jump');
+                $('#rodar').removeAttr('disabled');
+            }
+        }
+    });
+
+    // Showcase
+    slowSpeed($owl, 1400);
+
 });
 
-
-owl.on('changed.owl.carousel', function (e) {
-    currentItem = e.relatedTarget.current();
-    console.log("current: ",e.item.index) //same
-    console.log("total: ",e.item.count)   //total
-})
-
-
-
-function roll() {
-    if (rolling == true) { return 0; }
-    rolling = true;
-
-    owl.data('owl.carousel').options.autoplay = true;
-    owl.data('owl.carousel').options.autoplayTimeout = 100;
-    owl.trigger('refresh.owl.carousel' );
+/**
+ * Reduce speed roulette
+ * @param {type} $owl
+ * @param {type} speed
+ * @returns {undefined}
+ */
+function slowSpeed($owl, speed) {
+    $owl.trigger('play.owl.autoplay', [speed]);
 }
 
-/*
-let number = document.createElement("div");
-number.innerHTML = 0;
-number.classList.add("number");
-number.classList.add("owl-item");
-number.classList.add("green");
-document.getElementById("roleta").appendChild(number);
-
-for(let i=1; i++; i<=14) {
-let number = document.createElement("div");
-number.classList.add("number");
-number.classList.add("owl-item");
-if (i <= 7) { 
-    number.classList.add("red");
+/**
+ * Stop autoplay roulette
+ * @returns {undefined}
+ */
+function stopAutoplay() {
+    stoping = true;
 }
-else {
-    number.classList.add("black");
-}
-number.innerHTML = i;
-document.getElementById("roleta").appendChild(number);
-}
-*/
